4ec6cf7b221d651aa9ba201f2e5ef27480925ec8
ran-lovely-you <ran-miyoshi@outlook.jp>
2026-07-02 23:52:54 +0900

    Delete src directory

--- src/app/api/auth/callback/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-
-// implicitフローではコールバックは不要
-// マジックリンクはloginページで処理される
-export async function GET(request: NextRequest) {
-  const { origin } = new URL(request.url)
-  return NextResponse.redirect(`${origin}/login`)
-}
