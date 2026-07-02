1a2cbe43dc7cfc7baf1c08de1cb8aaca4727902d
ran-lovely-you <ran-miyoshi@outlook.jp>
2026-07-02 23:19:08 +0900

    Delete src directory

--- src/app/api/auth/callback/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-
-export async function GET(request: NextRequest) {
-  const { origin } = new URL(request.url)
-  return NextResponse.redirect(`${origin}/pricing`)
-}
